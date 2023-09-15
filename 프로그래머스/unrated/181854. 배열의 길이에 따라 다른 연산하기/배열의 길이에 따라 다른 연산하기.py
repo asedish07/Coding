b9fb4d0b91dbc621e4fa8e4b3612c27c14cbdcd1
asedish07 <128368531+asedish07@users.noreply.github.com>
2023-09-15 15:27:03 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -1,13 +1,15 @@
 def solution(arr, n):
     answer = []
-    is_odd_length = len(arr) % 2 == 1
-    
-    for i in range(len(arr)):
-        if is_odd_length and i % 2 == 0:
-            answer.append(arr[i] + n)
-        elif not is_odd_length and i % 2 != 0:
-            answer.append(arr[i] + n)
-        else:
-            answer.append(arr[i])
-            
+    if len(arr) % 2 == 0:
+        for i in range(len(arr)):
+            if i % 2 == 0:
+                answer.append(arr[i])
+            else:
+                answer.append(arr[i] + n)
+    else:
+        for i in range(len(arr)):
+            if i % 2 == 0:
+                answer.append(arr[i] + n)
+            else:
+                answer.append(arr[i])
     return answer
