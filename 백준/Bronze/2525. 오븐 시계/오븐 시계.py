47818d33656a8fd3c922d1af93a7412ae8af613f
asedish07 <128368531+asedish07@users.noreply.github.com>
2023-12-01 14:55:21 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,13 +1,7 @@
-hour, minute = map(int, input().split())
-time = input()
-time = int(time)
-if time+minute>60 and hour+((minute+time)//60)<=23:
-    hour = hour+((minute+time)//60)
-    minute = ((minute+time)%60)
-    print('%d %d' %(hour, minute))
-elif hour+((minute+time)//60)>23:
-    hour = hour+((minute+time)//60)-24
-    minute = ((minute+time)%60)
-    print('%d %d' %(hour, minute))
-else:
-    print('%d %d' %(hour, minute+time))
+t, m = map(int, input().split())
+ft = int(input())
+t += (ft + m) // 60
+m = (ft+m) % 60
+if t >= 24:
+  t -= 24
+print(f"{t} {m}")
