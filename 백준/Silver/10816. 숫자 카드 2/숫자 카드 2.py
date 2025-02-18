7af6388cf63e7773759eb9eb00092c8f140ee617
asedish07 <128368531+asedish07@users.noreply.github.com>
2025-02-18 18:35:55 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 772 ms, Memory: 115772 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,21 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+N_s = list(map(int, sys.stdin.readline().strip().split()))
+M = int(sys.stdin.readline().strip())
+M_s = list(map(int, sys.stdin.readline().strip().split()))
+
+n_dic = {}
+
+for n in N_s:
+  if n in n_dic:
+    n_dic[n] += 1
+  else:
+    n_dic[n] = 1
+
+for m in M_s:
+  if m in n_dic:
+    print(n_dic[m], end=' ')
+  else:
+     print(0, end=' ')
+ # 1시간 고민하고 블로그 봄. 개천재네;;
