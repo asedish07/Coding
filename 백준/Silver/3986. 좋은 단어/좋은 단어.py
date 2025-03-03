c76a161c3948164d94ecc1af7a918bdf17092eff
asedish07 <128368531+asedish07@users.noreply.github.com>
2025-03-03 23:18:25 +0900

    [Silver IV] Title: 좋은 단어, Time: 132 ms, Memory: 33976 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,18 @@
+n = int(input())
+ans = 0
+
+for _ in range(n):
+    stack = []
+    _list = list(input())
+    for i in _list:
+        if not len(stack):
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop(-1)
+        else:
+            stack.append(i)
+
+    if not len(stack):
+        ans += 1 
+
+print(ans)
